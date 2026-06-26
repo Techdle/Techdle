d0e28bf55edd11b6f4daf514255d71df4cc10b15
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-06-26 10:25:43 +0800

    Remove temporary Clear-Site-Data header to restore cache performance

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Clear-Site-Data",
-            value: "\"cache\"",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
